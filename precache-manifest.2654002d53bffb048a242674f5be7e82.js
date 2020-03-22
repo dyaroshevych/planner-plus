642069ea0765596426491a2eb48de56bef32d949
dyaroshevych <dyaroshevych@gmail.com>
2020-03-22 18:42:31 +0200

    Updates

---
+++ precache-manifest.2654002d53bffb048a242674f5be7e82.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae52cc27452416fee78e425bbb394d6e",
+    "url": "/planner-plus/index.html"
+  },
+  {
+    "revision": "bb9d72ffbb8472c23d97",
+    "url": "/planner-plus/static/css/main.5a28f5be.chunk.css"
+  },
+  {
+    "revision": "7220a9ff0a74300c6eb8",
+    "url": "/planner-plus/static/js/2.5a84f7c8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/planner-plus/static/js/2.5a84f7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb9d72ffbb8472c23d97",
+    "url": "/planner-plus/static/js/main.895c9349.chunk.js"
+  },
+  {
+    "revision": "d11a619be7634e5464ea",
+    "url": "/planner-plus/static/js/runtime-main.9b5fc077.js"
+  },
+  {
+    "revision": "51b6f612ee2ef4f17e1404c1478cfaae",
+    "url": "/planner-plus/static/media/close.51b6f612.svg"
+  },
+  {
+    "revision": "72322324ea4bb55127ca41a0ef41d438",
+    "url": "/planner-plus/static/media/edit.72322324.svg"
+  },
+  {
+    "revision": "77291a9764fe0385abf13c8aa602210c",
+    "url": "/planner-plus/static/media/menu.77291a97.svg"
+  },
+  {
+    "revision": "263881f52544b1b8cf2b5d892131815a",
+    "url": "/planner-plus/static/media/plus.263881f5.svg"
+  },
+  {
+    "revision": "b175098f2aa7157154cda1143e9bd89b",
+    "url": "/planner-plus/static/media/remove.b175098f.svg"
+  }
+]);
